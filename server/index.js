916210548ce717de1c602b6e2d93ebb53cd543da
AngujayalakshmiR <angujayalakshmi2005@gmail.com>
2025-07-30 14:59:52 +0530

    .

--- server/index.js
+++ server/index.js
@@ -1,31 +1,48 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
 mongoose.connect("mongodb+srv://myuser:mypassword@todo.ngqmi59.mongodb.net/?retryWrites=true&w=majority&appName=todo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
 const taskSchema = new mongoose.Schema({
-    text: String,
+  text: String,
 });
-const Task = mongoose.model("Task", taskSchema,"collection1");
+
+const Task = mongoose.model("Task", taskSchema, "collection1");
+
 app.get("/tasks", async (req, res) => {
-    const tasks = await Task.find();
-    res.json(tasks);
+  const tasks = await Task.find();
+  res.json(tasks);
 });
+
 app.post("/tasks", async (req, res) => {
-    const task = new Task({ text: req.body.text });
-    await task.save();
-    res.json(task);
+  const task = new Task({ text: req.body.text });
+  await task.save();
+  res.json(task);
 });
+
+app.put("/tasks/:id", async (req, res) => {
+  const updatedTask = await Task.findByIdAndUpdate(
+    req.params.id,
+    { text: req.body.text },
+    { new: true }
+  );
+  res.json(updatedTask);
+});
+
 app.delete("/tasks/:id", async (req, res) => {
-    await Task.findByIdAndDelete(req.params.id);
-    res.json({ message: "Task deleted" });
+  await Task.findByIdAndDelete(req.params.id);
+  res.json({ message: "Task deleted" });
 });
+
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
