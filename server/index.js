993bc2a154cc950a786674f16b535a896434d80b
AngujayalakshmiR <angujayalakshmi2005@gmail.com>
2025-07-30 10:20:54 +0530

    Initial MERN Todo app commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+mongoose.connect("mongodb+srv://myuser:mypassword@todo.ngqmi59.mongodb.net/?retryWrites=true&w=majority&appName=todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+const taskSchema = new mongoose.Schema({
+    text: String,
+});
+const Task = mongoose.model("Task", taskSchema,"collection1");
+app.get("/tasks", async (req, res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+app.post("/tasks", async (req, res) => {
+    const task = new Task({ text: req.body.text });
+    await task.save();
+    res.json(task);
+});
+app.delete("/tasks/:id", async (req, res) => {
+    await Task.findByIdAndDelete(req.params.id);
+    res.json({ message: "Task deleted" });
+});
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
